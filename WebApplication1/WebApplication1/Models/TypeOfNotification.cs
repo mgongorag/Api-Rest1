5110547c6c3b8422a339dafb5252113fb4061552
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-24 17:49:40 -0600

    Create TypeOfNotification.cs

---
+++ WebApplication1/WebApplication1/Models/TypeOfNotification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TypeOfNotification
+    {
+        [Key]
+        public int idType { get; set; }
+        [Required(ErrorMessage = "The name is required")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "The message is required")]
+        [StringLength(256, ErrorMessage = "Maximum characters is 100")]
+        public string message { get; set; }
+        public bool state { get; set; }
+        public TypeOfNotification()
+        {
+            this.state = true;
+        }
+
+    }
+}
