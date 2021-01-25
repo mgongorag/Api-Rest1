ac9be0ae73870cebc04c8fff058bb0f2cdcb3b02
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-24 22:08:28 -0600

    update names

---
+++ WebApplication1/WebApplication1/Models/Dtos/TypeOfNotificationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Dtos
+{
+    public class TypeOfNotificationDto
+    {
+        [Key]
+        public int idType { get; set; }
+        [Required(ErrorMessage = "The name is required")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "The message is required")]
+        [StringLength(256, ErrorMessage = "Maximum characters is 100")]
+        public string message { get; set; }
+        
+    }
+}
