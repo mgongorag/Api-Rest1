126216c7179316a4169080b2a5b5b4bf1210e326
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-24 22:09:36 -0600

    Create TypeOfNotificationController.cs

---
+++ WebApplication1/WebApplication1/Controllers/TypeOfNotificationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Dtos;
+using WebApplication1.Repository.IRepository;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/TypeOfNotification")]
+    [ApiController]
+    public class TypeOfNotificationController : Controller
+    {
+        private readonly ITypeOfNotificationRepository _tnRepo;
+        private readonly IMapper _mapper;
+
+        public TypeOfNotificationController(ITypeOfNotificationRepository tnrepo, IMapper mapper)
+        {
+            _tnRepo = tnrepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetTypesOfNotification()
+        {
+            var listTypes = _tnRepo.GetTypeOfNotifications();
+            var listTypesDto = new List<TypeOfNotificationDto>();
+            foreach(var list in listTypes)
+            {
+                listTypesDto.Add(_mapper.Map<TypeOfNotificationDto>(list));
+            }
+            return Ok(listTypesDto);
+        }
+
+    }
+}
