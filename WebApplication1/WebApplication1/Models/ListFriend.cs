ce02ca8c21ace8716b2d944117c27fe4173e9bcf
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-24 20:29:12 -0600

    Create ListFriend.cs

---
+++ WebApplication1/WebApplication1/Models/ListFriend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ListFriend
+    {
+        public int idUser { get; set; }
+        public int idFriend { get; set; }
+        public DateTime dateAdded { get; set; }
+        public bool state { get; set; }
+        [ForeignKey("idUser")]
+        public User user { get; set; }
+        [ForeignKey("idFriend")]
+        public User friend { get; set; }
+    }
+}
