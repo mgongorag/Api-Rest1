12d2f695114767908f10c512a272ab0604cf8579
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-24 20:17:57 -0600

    Create ImagePost.cs

---
+++ WebApplication1/WebApplication1/Models/ImagePost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ImagePost
+    {
+        [Key]
+        public int idImage { get; set; }
+        [Required]
+        public string path { get; set; }
+    }
+}
