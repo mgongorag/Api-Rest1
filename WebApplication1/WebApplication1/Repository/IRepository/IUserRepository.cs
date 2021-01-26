bb61ddcb25c0d382bba441fed666bf4c6070ede6
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-25 23:34:12 -0600

    Create IUserRepository.cs

---
+++ WebApplication1/WebApplication1/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Models.Dtos;
+
+namespace WebApplication1.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+
+        bool registerUser(User user);
+        bool existEmail(string email);
+        bool existUsername(string username);
+        User getUser(int idUSer);
+    }
+}
