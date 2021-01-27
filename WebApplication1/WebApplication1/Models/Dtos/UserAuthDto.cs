602629b567771ee43183c66bc28b81bf499fe243
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-01-26 20:36:57 -0600

    Create UserAuthDto.cs

---
+++ WebApplication1/WebApplication1/Models/Dtos/UserAuthDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Dtos
+{
+    public class UserAuthDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
