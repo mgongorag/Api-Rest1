d3174a5eab393dc1673d9aebc42150e31b643b8d
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-03 14:24:45 -0600

    Create IPostRepository.cs

---
+++ WebApplication1/WebApplication1/Repository/IRepository/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repository.IRepository
+{
+    interface IPostRepository
+    {
+        ICollection<Post> GetAllPostFromFriends(int idUser);
+        ICollection<Post> GetAllPostByIdFriend(int idUSer);
+        ICollection<Post> GetAllProfilePictures(int idUser);
+    }
+}
