05618e9928f64e41b87c490fd04fe9ec6fd64962
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-03 14:23:55 -0600

    Se implemento el metodo para obtener todos los post de un amigo

---
+++ WebApplication1/WebApplication1/Repository/PostRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.Repository.IRepository;
+
+namespace WebApplication1.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        public PostRepository (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICollection<Post> GetAllPostByIdFriend(int idUSer)
+        {
+            return _db.Post.Where(p => p.idUser == idUSer).OrderByDescending(p => p.date).ToList();
+        }
+
+        public ICollection<Post> GetAllPostFromFriends(int idUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Post> GetAllProfilePictures(int idUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
