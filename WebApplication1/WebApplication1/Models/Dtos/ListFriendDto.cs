005ca214800a2158bee67ffe91925df3a3b3174d
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-03 19:46:21 -0600

    Create ListFriendDto.cs

---
+++ WebApplication1/WebApplication1/Models/Dtos/ListFriendDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Dtos
+{
+    public class ListFriendDto
+    {
+        [Required(ErrorMessage = "Id user is required")]
+        [Key, Column(Order = 1)]
+        public int idUser { get; set; }
+        [Required(ErrorMessage = "Id friend is required")]
+        [Key, Column(Order = 2)]
+        public int idFriend { get; set; }
+       
+    }
+}
