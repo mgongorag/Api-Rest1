d54396ceae666971960281089383c063b894173e
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-03 19:46:00 -0600

    Se agrego el idPost

--- WebApplication1/WebApplication1/Models/ImagePost.cs
+++ WebApplication1/WebApplication1/Models/ImagePost.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace WebApplication1.Models
         [Required(ErrorMessage ="Path is required")]
         public string path { get; set; }
         public bool state { get; set; }
-
+        [ForeignKey("idPost")]
+        public int idPost { get; set; }
     }
 }
