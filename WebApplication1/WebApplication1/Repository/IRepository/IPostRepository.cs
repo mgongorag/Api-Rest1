5340021d92adf53f02d26da19a0d5c78127bb4de
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-06 13:06:12 -0600

    Update IPostRepository.cs

--- WebApplication1/WebApplication1/Repository/IRepository/IPostRepository.cs
+++ WebApplication1/WebApplication1/Repository/IRepository/IPostRepository.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using WebApplication1.Models.Dtos;
 
 namespace WebApplication1.Repository.IRepository
 {
     public interface IPostRepository
     {
-        ICollection<Post> GetAllPostFromFriends(int idUser);
-        ICollection<Post> GetAllPostByIdFriend(int idUSer);
+        List<PostDto> GetAllPostFromFriends(int idUser);
+        List<PostDto> GetAllPostByIdFriend(int idUSer);
         ICollection<Post> GetAllProfilePictures(int idUser);
+        bool AddPost(PostCreateDto post);
         Post GetPostById(int idPost);
+        bool Save();
     }
 }
