56d04280fc1d9b140b65b64b346b927bfb362205
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2021-02-06 13:06:05 -0600

    Se agregó la función para obtener Post por id Amigo y de todos los amigos

--- WebApplication1/WebApplication1/Repository/PostRepository.cs
+++ WebApplication1/WebApplication1/Repository/PostRepository.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Data;
 using WebApplication1.Models;
+using WebApplication1.Models.Dtos;
 using WebApplication1.Repository.IRepository;
 
 namespace WebApplication1.Repository
@@ -18,17 +20,148 @@ namespace WebApplication1.Repository
             _db = db;
         }
 
-        public ICollection<Post> GetAllPostByIdFriend(int idUSer)
+        public bool AddPost(PostCreateDto post)
         {
-            return _db.Post.Where(p => p.idUser == idUSer)
-                           .Include(p => p.typeOfPost)
-                           .Include(p => p.user)
-                           .OrderByDescending(p => p.date).ToList();
+            using var transaction = _db.Database.BeginTransaction();
+            bool success = false;
+
+            Post postDb = new Post();
+            postDb.description = post.description;
+            postDb.idUser = post.idUser;
+            postDb.idTypePost = post.idTypePost;
+
+            try
+            {
+                _db.Post.Add(postDb);
+                success = Save();
+
+                if(post.images != null)
+                {
+                    foreach (var image in post.images)
+                    {
+                        var imageDB = new ImagePost();
+                        imageDB.idPost = postDb.idPost;
+                        imageDB.path = image.path;
+                        _db.Add(imageDB);
+                        success = Save();
+                    }
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                success = false;
+            }
+            return success;
         }
 
-        public ICollection<Post> GetAllPostFromFriends(int idUser)
+
+        public bool Save()
         {
-            throw new NotImplementedException();
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public List<PostDto> GetAllPostByIdFriend(int idUSer)
+        {
+            List<PostDto> PostOfFriend = new List<PostDto>();
+             
+            using(var db = _db)
+            {
+                PostOfFriend = (from d in db.Post
+                                where d.idUser == idUSer
+                                orderby d.date descending
+                                select new PostDto
+                                {
+                                    idPost = d.idPost,
+                                    description = d.description,
+                                    images = (
+                                        from i in db.ImagePost
+                                        where i.idPost == d.idPost
+                                        && i.state == true
+                                        orderby i.idImage
+                                        select new ImagePostDto
+                                        {
+                                            idImage = i.idImage,
+                                            path = i.path,
+                                            idPost = i.idPost
+                                        }
+                                    ).ToList(),
+                                    user = (
+                                                from u in db.User
+                                                where u.idUSer == d.idUser
+                                                select new PostUserDto
+                                                {
+                                                    idUSer = u.idUSer,
+                                                    firstName = u.firstName,
+                                                    lastName = u.lastName,
+                                                    username = u.username
+                                                }
+                                            ).FirstOrDefault(),
+                                    typeOfPost = (
+                                                    from t in db.TypeOfPost
+                                                    where t.idTypeOfPost == d.idTypePost
+                                                    select new TypeOfPost
+                                                    {
+                                                        idTypeOfPost = t.idTypeOfPost,
+                                                        typePost = t.typePost
+                                                    }
+                                                 ).FirstOrDefault(),
+                                }).ToList();
+                                   
+            }
+            return PostOfFriend;
+        }
+
+        public List<PostDto> GetAllPostFromFriends(int idUser)
+        {
+            List<PostDto> PostOfFriend = new List<PostDto>();
+
+            using (var db = _db)
+            {
+                PostOfFriend = (from u in db.User
+                                join lf in db.ListFriend on u.idUSer equals lf.idUser
+                                join friend in db.User on lf.idFriend equals friend.idUSer
+                                join post in db.Post on lf.idFriend equals post.idUser
+                                //join img in db.ImagePost on post.idPost equals img.idPost
+                                where u.idUSer == idUser
+                                select new PostDto
+                                {
+                                    idPost = post.idPost,
+                                    description = post.description,
+                                    images = (
+                                            from img in db.ImagePost
+                                            where img.idPost == post.idPost
+                                            select new ImagePostDto
+                                            {
+                                                idImage = img.idImage,
+                                                path = img.path,
+                                                idPost = img.idPost
+                                            }).ToList(),
+                                    typeOfPost = (
+                                                    from t in db.TypeOfPost
+                                                    where t.idTypeOfPost == post.idTypePost
+                                                    select new TypeOfPost
+                                                    {
+                                                        idTypeOfPost = t.idTypeOfPost,
+                                                        typePost = t.typePost
+                                                    }
+                                                 ).FirstOrDefault(),
+                                    user = (
+                                                from u in db.User
+                                                where u.idUSer == friend.idUSer
+                                                select new PostUserDto
+                                                {
+                                                    idUSer = u.idUSer,
+                                                    firstName = u.firstName,
+                                                    lastName = u.lastName,
+                                                    username = u.username
+                                                }
+                                            ).FirstOrDefault()
+                                }).ToList();
+            }
+            return PostOfFriend;
         }
 
         public ICollection<Post> GetAllProfilePictures(int idUser)
